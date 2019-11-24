38ebf1e6c2d1d8263a1c2a417c0844a95849a5e0
Craig Donavin <crdonavin@gmail.com>
2019-11-24 16:16:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+let createEmployeeRecord = function(srcArray) {
+  // the tests seem to describe an object
+  return {
+    firstName: srcArray[0],
+    familyName: srcArray[1],
+    title: srcArray[2],
+    payPerHour: srcArray[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+let createEmployeeRecords = function(arr) {
+    return arr.map(function(el){
+      return createEmployeeRecord(el)
+    })
+}
+
+let createTimeInEvent = function(employee, time) {
+  let [date, hour] = time.split(' ')
+
+  employee.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date
+  })
+  return employee
+}
